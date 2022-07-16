4a4ba054a48f46017cc7553a742a95421bb08173
unimexes2 <unimexes2@gmail.com>
2022-07-16 14:52:46 +0200

    1st iteration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import beers from "./assets/beers.png"
+import newbeer from "./assets/new-beer.png"
+import randombeer from "./assets/random-beer.png"
 import './App.css';
 
 function App() {
+ 
+ 
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a style={{ display: "flex", margin: "auto" }} href="/beers">
+        <div className="container">
+          <img src={beers} />
+          <div className="content">
+             <h4>Beers</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              q </p>
+          </div>
+
+        </div>
+
+      </a>
+      <a style={{ display: "flex", margin: "auto" }} href="/newbeer">
+        <div className="container">
+          <img src={newbeer} />
+          <div className="content">
+          <h4>New Beer</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              q </p>
+          </div>
+
+        </div>
+
+      </a>
+      <a style={{ display: "flex", margin: "auto" }} href="/randombeer">
+        <div className="container">
+          <img src={randombeer} />
+          <div className="content">
+          <h4>Random Beer</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              q </p>
+          </div>
+       </div>
+      </a>
+
+
+      <div>
+     <Routes>
+     <Route path="/countries" element={<CountriesList countries={countriesData} />}
+            />
+    
+    <Route path="/countries/:countriesId"  element={<CountryPage />} />
+    
+     </Routes>
+     </div>
+
+
+
+
     </div>
   );
 }
